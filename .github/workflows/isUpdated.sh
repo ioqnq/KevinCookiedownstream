043f8913b3b4590a9a789423f600f7ef043fce48
Nedelcu Ioana <ioana.nedelcu643@gmail.com>
2025-07-08 14:54:54 +0300

    Update isUpdated.sh

--- .github/workflows/isUpdated.sh
+++ .github/workflows/isUpdated.sh
@@ -2,14 +2,16 @@
 
 TARGET_FILE="tock-tbf/src"
 
-DAYS_SINCE_COMMIT=$(curl -s "url -s https://api.github.com/repos/ioqnq/KevinCookieCompany/commits?path=$TARGET_FILE&sha=REL1_36" \
+DAYS_SINCE_COMMIT=$(curl -s "https://api.github.com/repos/ioqnq/KevinCookieCompany/commits?path=$TARGET_FILE&sha=REL1_36" \
   | jq -r ".[0].commit.author.date" \
   | xargs -I{} date -d {} +%s)
 
 NOW=$(date +%s)
+
 DIFF_DAYS=$(( (NOW - DAYS_SINCE_COMMIT) / 60 / 60 / 24 ))
-echo $DIFF_DAYS
-DIFF_MINS=$(( (NOW - DAYS_SINCE_COMMIT) / 60))
-echo $DIFF_MINS
+DIFF_MINS=$(( (NOW - DAYS_SINCE_COMMIT) / 60 ))
+
+echo "⏱️ Age in days: $DIFF_DAYS"
+echo "⏱️ Age in minutes: $DIFF_MINS"
 
 exit $DIFF_DAYS
