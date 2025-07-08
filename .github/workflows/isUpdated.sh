2d6a5e395205e616b245cb8de47eb0baa93f3bce
Nedelcu Ioana <ioana.nedelcu643@gmail.com>
2025-07-08 14:40:18 +0300

    Update isUpdated.sh
    Based on file path

--- .github/workflows/isUpdated.sh
+++ .github/workflows/isUpdated.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-exit $(curl -s https://api.github.com/repos/ioqnq/KevinCookieCompany.com/commits/main | jq -r "((now - (.commit.author.date | fromdateiso8601) )  / (60*60*24)  | trunc)")
+
+TARGET_FILE="tock-tbf/src"
+
+DAYS_SINCE_COMMIT=$(curl -s "url -s https://api.github.com/repos/ioqnq/KevinCookieCompany/commits?path=$TARGET_FILE&sha=REL1_36" \
+  | jq -r ".[0].commit.author.date" \
+  | xargs -I{} date -d {} +%s)
+
+NOW=$(date +%s)
+DIFF_DAYS=$(( (NOW - DAYS_SINCE_COMMIT) / 60 / 60 / 24 ))
+
+exit $DIFF_DAYS
